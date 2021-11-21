83c9dceddced3d728b3d064466ba6a1daabc7638
YinWang3026 <yw2178@nyu.edu>
2021-11-21 18:38:33 -0500

    fixed some bugs

--- apps/project/lib/merkletree.ex
+++ apps/project/lib/merkletree.ex
@@ -1,30 +1,6 @@
-# iex(6)> <<5::3>> == <<5::3>>
-# true
-# iex(7)> <<5::3>> == <<4::3>> 
-# false
-# iex(8)> <<5::3>> < <<4::3>> 
-# false
-# iex(9)> <<5::3>> > <<4::3>>
-# true
-# iex(10)>
-# nil
-# iex(11)> x =  :crypto.hash(:md5, "hi")
-# <<73, 246, 138, 92, 132, 147, 236, 44, 11, 244, 137, 130, 28, 33, 252, 59>>
-# iex(12)> y =  :crypto.hash(:md5, "hello")
-# <<93, 65, 64, 42, 188, 75, 42, 118, 185, 113, 157, 145, 16, 23, 197, 146>>
-# iex(13)> x == y
-# false
-# iex(14)> x === y
-# false
-# iex(15)> x < y
-# true
-# iex(16)> x > y 
-# false
-
-## Note this is Binary Search Tree
-## May or may not be balanced
-## Each Node holds a hash, which is the combined hash of itself and its children
-## Each Node holds a value, which is the hash value of some value stored in map
+## Tree is built from bottom up and left to right
+## Leaves are the hashes of some values
+## Each internal node is the md5(left_child <> right_child)
 
 defmodule MerkleTree do
     @moduledoc """
@@ -40,7 +16,7 @@ defmodule MerkleTree do
     )
 
     ## Level 0 = Data nodes
-    ## root_level has root node
+    ## root_level has the root node
 
     @doc """
     Return an empty Merkle Tree, this is mostly
@@ -55,53 +31,63 @@ defmodule MerkleTree do
         %MerkleTree{ matrix: [[]], root_level: 0, leaf_count: 0 }
     end
 
+    ## Given a matrix, row, and a value
+    ## Returns a matrix with the value appended to the row
     defp append_to_row(matrix, row, value) do
         List.replace_at(matrix, row, Enum.at(matrix, row) ++ [value])
     end
-
-    defp append_to_matrix(matrix, value) do
-        matrix ++ [value]
-    end
-
+    
+    ## Given a matrix, row, and col
+    ## Returns the item at row, col in the matrix
     defp get_item_at(matrix, row, col) do
         Enum.at(Enum.at(matrix, row), col)
     end
 
+    ## Given a matrix, row, col, and value
+    ## Returns a matrix with value at row, col
     defp replace_item_at(matrix, row, col, value) do
         row_list = List.replace_at(Enum.at(matrix, row), col, value)
         List.replace_at(matrix, row, row_list)
     end
 
+    ## Given 2 hashes, h1 and h2
+    ## Return if h2 == nil, return h1, else return md5(h1 <> h2)
+    defp concatenate_hash(h1, h2) do
+        if h2 == nil do
+            h1
+        else
+            :crypto.hash(:md5, h1 <> h2)
+        end
+    end
+
     defp insert_rec(matrix, row, col) do
+        # IO.puts("matrix inspection #{inspect(matrix)}")
         if length(matrix) <= row do
-            # Need to add a row
+            ## Need to add a row
+            ## Generally, this is a top/root, so we are done
             h1 = get_item_at(matrix, row-1, col*2)
             h2 = get_item_at(matrix, row-1, col*2+1)
-            if h2 == nil do
-                new_matrix = matrix ++ [[h1]]
-                {new_matrix, row}
-            else
-                new_hash = :crypto.hash(:md5, h1 <> h2)
-                new_matrix = matrix ++ [[new_hash]]
-                {new_matrix, row}
-            end
+            new_hash = concatenate_hash(h1, h2)
+            new_matrix = matrix ++ [[new_hash]]
+            {new_matrix, row}
         else
-            # Row exists
+            ## Row exists
             if length(Enum.at(matrix, row)) <= col do
-                # Parent node does not exist
+                ## Parent node does not exist, add it, and copy its child hash
                 child_hash = get_item_at(matrix, row-1, col*2)
-                matrix = append_to_row(matrix, row, child_hash)
-                insert_rec(matrix, row+1, Integer.floor_div(col, 2))
+                new_matrix = append_to_row(matrix, row, child_hash)
+                insert_rec(new_matrix, row+1, Integer.floor_div(col, 2))
             else
-                # Parent node does exist
+                ## Parent node does exist
+                ## Recalculate its hash
                 h1 = get_item_at(matrix, row-1, col*2)
-                h2 = get_item_at(matrix, row-1, col*2+1)
-                new_hash = :crypto.hash(:md5, h1 <> h2)
-                matrix = replace_item_at(matrix, row, col, new_hash)
-                if length(Enum.at(matrix, row)) == 1 do
-                    {matrix, row}
+                h2 = get_item_at(matrix, row-1, col*2+1) # h2 might be nil, so only take on h1
+                new_hash = concatenate_hash(h1, h2)
+                new_matrix = replace_item_at(matrix, row, col, new_hash)
+                if length(Enum.at(new_matrix, row)) == 1 do
+                    {new_matrix, row}
                 else
-                    insert_rec(matrix, row+1, Integer.floor_div(col, 2))
+                    insert_rec(new_matrix, row+1, Integer.floor_div(col, 2))
                 end
             end
         end
@@ -117,12 +103,13 @@ defmodule MerkleTree do
         leaf_count: non_neg_integer()
     }
     def insert(tree, bytes) when is_binary(bytes) do
-        IO.puts("Insert ...")
+        IO.puts("Inserting ...")
         IO.inspect(bytes, binaries: :as_binaries)
 
         ## Add the new value to 0th row
         new_matrix = append_to_row(tree.matrix, 0, bytes)
         # IO.puts("new_matrix #{inspect(new_matrix)}")
+
         ## Increase the count
         new_leaf_count = tree.leaf_count + 1
 
@@ -132,13 +119,12 @@ defmodule MerkleTree do
         
         ## Recursively fix the tree
         {new_matrix, new_root_level} = insert_rec(new_matrix, parent_row, parent_col)
-        
+        # IO.puts("new_matrix #{inspect(new_matrix)} new_root_level #{inspect(new_root_level)}")
+
         ## Update
         tree = %{ tree | matrix: new_matrix }
         tree = %{ tree | leaf_count: new_leaf_count }
         tree = %{ tree | root_level: new_root_level }
-        # IO.puts("done_matrix #{inspect(new_matrix)}")
-        tree
     end
 
     @doc """
@@ -147,14 +133,17 @@ defmodule MerkleTree do
     @spec get_root_hash(%MerkleTree{}) :: :no_root | binary()
     def get_root_hash(tree) do
         if tree.root_level == 0 do
+            ## Empty tree, no root
             :no_root
         else
-            # Return the first item on root_level
+            ## Return the first item on root_level
             root_list = Enum.at(tree.matrix, tree.root_level)
-            if length(root_list) == 0 do # Error check in case
+            if length(root_list) != 1 do 
+                ## Error check in case
                 :no_root
-            else # Root exists
-                hd(root_list)
+            else 
+                ## Root exists, get the first item in the list
+                Enum.at(root_list, 0)
             end
         end
     end
--- apps/project/test/merkletree_test.exs
+++ apps/project/test/merkletree_test.exs
@@ -26,6 +26,9 @@ defmodule MerkleTreeTest do
         h3 = :crypto.hash(:md5, <<"THIS IS DIST SYS">>) # <<202, 131, 189, 86, 8, 5, 202, 50, 31, 118, 229, 137, 204, 6, 23, 78>>
         h4 = :crypto.hash(:md5, <<"PROJECT DYNAMO">>) # <<33, 227, 233, 6, 27, 75, 153, 23, 0, 222, 243, 76, 127, 212, 173, 75>>
         h5 = :crypto.hash(:md5, <<12,23,45,56>>) # <<212, 84, 233, 244, 41, 222, 8, 168, 214, 184, 144, 186, 6, 226, 191, 226>>
+        h6 = :crypto.hash(:md5, "What is up") # <<219, 174, 69, 8, 139, 82, 240, 216, 90, 28, 137, 111, 236, 207, 197, 58>>
+        h7 = :crypto.hash(:md5, "Full Merkle tree test") # <<229, 140, 75, 230, 164, 73, 63, 23, 154, 128, 216, 50, 5, 43, 128, 46>>
+        h8 = :crypto.hash(:md5, "1+1=3 maybe") # <<196, 55, 230, 34, 27, 73, 69, 116, 132, 119, 132, 185, 151, 245, 15, 112>>
 
         # Insert item 1
         aTree = MerkleTree.insert(aTree, h1)
@@ -70,7 +73,37 @@ defmodule MerkleTreeTest do
         assert aTree.leaf_count == 5
         root_hash = MerkleTree.get_root_hash(aTree)
         assert root_hash == :crypto.hash(:md5, h1_h2_h3_h4 <> h5)
-        
+
+        # Insert item 6
+        aTree = MerkleTree.insert(aTree, h6)
+        assert length(aTree.matrix) == 4
+        assert aTree.root_level == 3
+        assert aTree.leaf_count == 6
+        root_hash = MerkleTree.get_root_hash(aTree)
+        h5_h6 = :crypto.hash(:md5, h5 <> h6)
+        h1_h2_h3_h4_h5_h6 = :crypto.hash(:md5, h1_h2_h3_h4 <> h5_h6)
+        assert root_hash == h1_h2_h3_h4_h5_h6
+
+        # Insert item 7
+        aTree = MerkleTree.insert(aTree, h7)
+        assert length(aTree.matrix) == 4
+        assert aTree.root_level == 3
+        assert aTree.leaf_count == 7
+        root_hash = MerkleTree.get_root_hash(aTree)
+        h5_h6_h7 = :crypto.hash(:md5, h5_h6 <> h7)
+        h1_h2_h3_h4_h5_h6_h7 = :crypto.hash(:md5, h1_h2_h3_h4 <> h5_h6_h7)
+        assert root_hash == h1_h2_h3_h4_h5_h6_h7
+
+        # Insert item 8
+        aTree = MerkleTree.insert(aTree, h8)
+        assert length(aTree.matrix) == 4
+        assert aTree.root_level == 3
+        assert aTree.leaf_count == 8
+        root_hash = MerkleTree.get_root_hash(aTree)
+        h7_h8 = :crypto.hash(:md5, h7 <> h8)
+        h5_h6_h7_h8 = :crypto.hash(:md5, h5_h6 <> h7_h8)
+        assert root_hash == :crypto.hash(:md5, h1_h2_h3_h4 <> h5_h6_h7_h8)
+
         # Insert none binary item
         assert_raise FunctionClauseError, fn ->
             MerkleTree.insert(aTree, 12234556) # Inserting something not a byte
