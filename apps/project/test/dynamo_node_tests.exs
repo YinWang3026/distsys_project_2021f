c5331d99642b7a59884d28b2ff0ab5a0927be168
Rahul Sankar <rahulsnkr@gmail.com>
2021-12-05 12:09:22 -0500

    Added soome more tests

--- apps/project/test/dynamo_node_tests.exs
+++ apps/project/test/dynamo_node_tests.exs
@@ -283,5 +283,164 @@ defmodule DynamoNodeTest do
 
     assert {[49], _context} = Map.get(pref_2_state.store, :foo)
   end
+    test "Put request overwrites key in initial data" do
+      DynamoUtils.new_cluster(
+        %{foo: 37},
+        [:a, :b, :c],
+        1,
+        1,
+        1,
+        1_000,
+        1_000,
+        9999,
+        500,
+        700
+      )
+  
+      nonce_put = DynamoUtils.generate_nonce()
+      nonce_get = DynamoUtils.generate_nonce()
+  
+      send(:a, %ClientPutRequest{
+        nonce: nonce_put,
+        key: :foo,
+        value: 42,
+        context: %Context{version: VectorClock.new()}
+      })
+  
+      send(:a, %ClientGetRequest{nonce: nonce_get, key: :foo})
+  
+      assert_receive %ClientGetResponse{
+                       nonce: ^nonce_get,
+                       success: true,
+                       values: [42],
+                       context: _context
+                     },
+                     5_000
+    end
+  
+    test "Simple get with multiple nodes" do
+      data = %{foo: 39, bar: 42, baz: 47}
+  
+      nodes = [:a, :b, :c, :d, :e, :f]
+  
+      DynamoUtils.new_cluster(data, nodes, 4, 3, 2, 1_000, 1_000, 9999, 500, 700)
+  
+      for {key, value} <- data do
+        nonce = DynamoUtils.generate_nonce()
+  
+        send(:c, %ClientGetRequest{nonce: nonce, key: key})
+  
+        assert_receive %ClientGetResponse{
+                         nonce: ^nonce,
+                         success: true,
+                         values: [^value],
+                         context: _context
+                       },
+                       5_000
+      end
+    end
+  
+    test "Simple put and get with multiple nodes" do
+      data = %{foo: 39, bar: 42, baz: 47}
+  
+      nodes = [:a, :b, :c, :d, :e, :f]
+  
+      DynamoUtils.new_cluster(data, nodes, 4, 3, 2, 1_000, 1_000, 9999, 500, 700)
+  
+      for {key, _value} <- data do
+        nonce = DynamoUtils.generate_nonce()
+  
+        send(:c, %ClientPutRequest{
+          nonce: nonce,
+          key: key,
+          value: 100,
+          context:  %Context{version: VectorClock.new()}
+        })
+      end
+  
+      # wait a bit for the system to settle
+      wait(1_000)
+  
+      for key <- Map.keys(data) do
+        nonce = DynamoUtils.generate_nonce()
+  
+        send(:e, %ClientGetRequest{nonce: nonce, key: key})
+  
+        assert_receive %ClientGetResponse{
+                         nonce: ^nonce,
+                         success: true,
+                         values: [100],
+                         context: _context
+                       },
+                       5_000
+      end
+    end
+  
+    test "Client get request times out after a while when sent to coordinator" do
+      # make sure the node we send to is a valid coordinator
+      # by making everyone a valid coordinator
+      spawn(:a, fn ->
+        DynamoNode.init(
+          :a,
+          %{foo: 42},
+          [:a, :b, :c],
+          3,
+          3,
+          3,
+          500,
+          1_000,
+          9999,
+          500,
+          700
+        )
+      end)
+  
+      nonce = DynamoUtils.generate_nonce()
+      send(:a, %ClientGetRequest{nonce: nonce, key: :foo})
+  
+      assert_receive %ClientGetResponse{
+                       nonce: ^nonce,
+                       success: false,
+                       values: nil,
+                       context: nil
+                     },
+                     1_000
+    end
+  
+    test "Client put request times out after a while when sent to coordinator" do
+      # make sure the node we send to is a valid coordinator
+      # by making everyone a valid coordinator
+      spawn(:a, fn ->
+        DynamoNode.init(
+          :a,
+          %{foo: 42},
+          [:a, :b, :c],
+          3,
+          3,
+          3,
+          500,
+          1_000,
+          9999,
+          500,
+          700
+        )
+      end)
+  
+      nonce = DynamoUtils.generate_nonce()
+  
+      send(:a, %ClientPutRequest{
+        nonce: nonce,
+        key: :foo,
+        value: 49,
+        context: new_context()
+      })
+  
+      assert_receive %ClientPutResponse{
+                       nonce: ^nonce,
+                       success: false,
+                       context: nil
+                     },
+                     1_000
+    end
 end
   
